7940969ce7c7e72c4581c991d2b0978572cb4933
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-01-14 11:35:46 -0800

    clean up fountain solution

--- 2D Arrays and Graph Problems/fontanBFS.cpp
+++ 2D Arrays and Graph Problems/fontanBFS.cpp
@@ -6,30 +6,34 @@ int main(){
     int rMax, cMax; cin >> rMax >> cMax;
     vector<string> grid(rMax);
     for(string& s : grid) cin >> s;
-    deque<pair<int,int>> q;
-    for(int i = 0; i < rMax; i++){
+
+    queue<pair<int,int>> q;
+    for(int i = 0; i < rMax - 1; i++){
         for(int j = 0; j < cMax; j++){
             if(grid[i][j] != 'V') continue;
-            if(i < rMax - 1 && grid[i + 1][j] == 'V') continue;
-            q.push_back({i, j});
+            if(grid[i + 1][j] == 'V') continue;
+            // push all water coordinates 
+            // that are not in the last row
+            // and don't have a water block underneath already
+            q.push({i, j});
         }
     }
     while(!q.empty()){
         // using structured bindings to rename pair<int,int>
         auto [i, j] = q.front();
-        q.pop_front();
+        q.pop();
         if(i == rMax - 1) continue;
         if(grid[i + 1][j] == '.'){
             grid[i + 1][j] = 'V';
-            q.push_back({i + 1, j});
+            q.push({i + 1, j});
         } else if(grid[i + 1][j] == '#'){
             if(j > 0 && grid[i][j - 1] == '.'){
                 grid[i][j - 1] = 'V';
-                q.push_back({i, j - 1});
+                q.push({i, j - 1});
             }
             if(j + 1 < cMax && grid[i][j + 1] == '.'){
                 grid[i][j + 1] = 'V';
-                q.push_back({i, j + 1});
+                q.push({i, j + 1});
             }
         }
     }
--- 2D Arrays and Graph Problems/fontanDFS.cpp
+++ 2D Arrays and Graph Problems/fontanDFS.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int rMax, cMax;
+vector<string> grid;
 
-void dfs(vector<string>& grid, int i, int j){
+void dfs(int i, int j){
     if(i == rMax - 1 || grid[i + 1][j] == 'V') return;
     if(grid[i + 1][j] == '.'){
         grid[i + 1][j] = 'V';
-        dfs(grid, i + 1, j);
-    } else {
+        dfs(i + 1, j);
+    } else if(grid[i + 1][j] == '#'){
         if(j > 0 && grid[i][j - 1] == '.'){
             grid[i][j - 1] = 'V';
-            dfs(grid, i, j - 1);
+            dfs(i, j - 1);
         }
         if(j + 1 < cMax && grid[i][j + 1] == '.'){
             grid[i][j + 1] = 'V';
-            dfs(grid, i, j + 1);
+            dfs(i, j + 1);
         }
     }
 }
@@ -23,14 +24,19 @@ void dfs(vector<string>& grid, int i, int j){
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
     cin >> rMax >> cMax;
-    vector<string> grid(rMax);
+    grid.resize(rMax);
     for(string& s : grid) cin >> s;
+
+    // water falls off bottom of grid, so rMax - 1
     for(int i = 0; i < rMax - 1; i++){
         for(int j = 0; j < cMax; j++){
             if(grid[i][j] != 'V') continue;
             if(grid[i + 1][j] == 'V') continue;
-            dfs(grid, i, j);
+            // dfs on all water blocks not in last row
+            // if the block underneath is not water already
+            dfs(i, j);
         }
     }
+
     for(string s : grid) cout << s << '\n';
 }
